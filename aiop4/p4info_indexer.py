4a429e6aca8252a44a4476588a6f497884dc1ef3
Vinicius Arcanjo <viniarck@gmail.com>
2022-08-17 00:28:50 -0300

    Added P4InfoIndexer

---
+++ aiop4/p4info_indexer.py
@@ -0,0 +1,39 @@
+from p4.config.v1.p4info_pb2 import P4Info
+
+
+class P4InfoIndexer:
+
+    """P4InfoIndexer."""
+
+    def __init__(self, p4info: P4Info, iter_attr="name") -> None:
+        """P4InfoIndexer."""
+        self.p4info: P4Info = p4info
+
+        self.tables = self._index_by_preamble("tables", iter_attr)
+        self.actions = self._index_by_preamble("actions", iter_attr)
+        self.action_profiles = self._index_by_preamble("action_profiles", iter_attr)
+        self.counters = self._index_by_preamble("counters", iter_attr)
+        self.direct_counters = self._index_by_preamble("direct_counters", iter_attr)
+        self.meters = self._index_by_preamble("meters", iter_attr)
+        self.direct_meters = self._index_by_preamble("direct_meters", iter_attr)
+        self.value_sets = self._index_by_preamble("value_sets", iter_attr)
+        self.registers = self._index_by_preamble("digests", iter_attr)
+        self.digests = self._index_by_preamble("digests", iter_attr)
+        self.externs = self._index_by_preamble("externs", iter_attr)
+        self.table_match_fields = self._index_by_table_match_fields(iter_attr)
+
+    def _index_by_table_match_fields(self, iter_attr="name") -> dict:
+        """index by table_match_fields."""
+        table_match_fields = {}
+        for table_name, table in self.tables.items():
+            for match_field in table.match_fields:
+                key = (table_name, getattr(match_field, iter_attr))
+                table_match_fields[key] = match_field
+        return table_match_fields
+
+    def _index_by_preamble(self, iter_name: str, iter_attr="name") -> dict:
+        """Index an iterable by preamble name."""
+        return {
+            getattr(item.preamble, iter_attr): item
+            for item in getattr(self.p4info, iter_name, [])
+        }
