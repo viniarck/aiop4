f42ecaa7b5eda8772f4f723d91fab56f33205211
Vinicius Arcanjo <viniarck@gmail.com>
2022-08-20 16:36:20 -0300

    Added more unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,6 @@ def client() -> Client:
     """aoip4 Client."""
     client = Client()
     client._stub = AsyncMock()
+    client._stream_channel = AsyncMock()
     client._channel = AsyncMock()
     return client
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,6 @@
+from unittest.mock import AsyncMock
+
+import grpc
 import p4.v1.p4runtime_pb2 as p4r_pb2
 
 
@@ -14,3 +17,33 @@ async def test_get_fwd_pipeline(client) -> None:
     assert client._stub.GetForwardingPipelineConfig.call_count == 1
     arg = p4r_pb2.GetForwardingPipelineConfigRequest(device_id=client.device_id)
     client._stub.GetForwardingPipelineConfig.assert_called_with(arg)
+
+
+def test_is_primary(client) -> None:
+    """Test is_primary."""
+    assert not client.is_primary()
+    client._is_primary.set()
+    assert client.is_primary()
+
+
+async def test_try_to_become_primary(client) -> None:
+    """Test try_to_become_primary."""
+    client._stream_channel.read.return_value = p4r_pb2.StreamMessageResponse(
+        arbitration=p4r_pb2.MasterArbitrationUpdate()
+    )
+    client.stream_control = AsyncMock()
+    await client.try_to_become_primary()
+    assert client.stream_control.call_count == 1
+    assert client.stream_channel.read.call_count == 1
+    assert client._stream_control_task
+    assert client.is_primary()
+
+
+async def test_stream_control(client) -> None:
+    """Test stream_control."""
+    client._stream_channel.read.side_effect = [
+        p4r_pb2.StreamMessageResponse(arbitration=p4r_pb2.MasterArbitrationUpdate()),
+        grpc.aio.EOF,
+    ]
+    await client.stream_control()
+    assert client.stream_channel.read.call_count == 2
